c118353aedc06c41fb1751b85367363bdc529a01
El_Nico <118479527+NicoRoso@users.noreply.github.com>
2023-09-22 18:51:11 +0300

    Enemy With Models

--- Assets/TestEnemy/Enemy.cs
+++ Assets/TestEnemy/Enemy.cs
@@ -11,6 +11,9 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] private float _speedBullet;
     [SerializeField] private float _fireRate;
+    [SerializeField] private float _deathTime;
+
+    [SerializeField] private BoxCollider box;
 
     private GameObject _player;
     private float _nextFireTime;
@@ -26,7 +29,10 @@ public class Enemy : MonoBehaviour
 
         if(_hp <= 0 )
         {
-            Destroy(this.gameObject);
+            _fireRate = 0;
+            Destroy(this.box);
+            GetComponent<Animator>().SetTrigger("Death");
+            Death();
         }
 
         transform.LookAt(_player.transform.position);
@@ -40,9 +46,15 @@ public class Enemy : MonoBehaviour
 
     private void Fire()
     {
+        GetComponent<Animator>().SetTrigger("Fire");
         GameObject bullet = Instantiate(_bullet, _spawnPoint.position, Quaternion.identity);
         Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
         bulletRigidbody.velocity = (_player.transform.position - _spawnPoint.position).normalized * _speedBullet;
         bulletRigidbody.useGravity = false;
+    }
+
+    private void Death()
+    {
+        Destroy(this.gameObject, _deathTime);
     }
 }
