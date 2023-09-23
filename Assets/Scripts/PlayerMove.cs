b76c44ca35eebe2299a2cd5dfc29ad25cb66fdae
actuallynotsarcasm <timofeysimonov@mail.ru>
2023-09-23 16:32:45 +0300

    Jetpack and grappling hook but still with bugs

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -13,12 +13,41 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float cy;
     [SerializeField] private float mouseSensivity;
 
+    [SerializeField] private GameObject hook;
+    [SerializeField] private LineRenderer rope;
 
+    private float angle;
+    private Rigidbody rb;
+    private float releaseTimer;
+    private float cooldownTimer;
+    [SerializeField] private float cooldownTime;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private float jetpackPower;
+    [SerializeField] private float jetpackHorizontalPower;
+    [SerializeField] private float gravityPower;
+    private Vector3 lastSpeed;
+    public bool hookGrabbed;
+    private bool wasGrabbed;
+    [SerializeField] private float hookMaxLenght;
+    private float hookLenght;
+    [SerializeField] private float hookSpeed;
+    private bool hookActive;
+    private Vector3 hookDirection;
+    private Vector3 currentSpeed;
+    [SerializeField] private float jetpackFuel;
+    [SerializeField] private float maxFuel;
     [SerializeField] private bool _canJump;
 
     void Start()
     {
+        cooldownTimer = Time.realtimeSinceStartup - cooldownTime;
+        rb = GetComponent<Rigidbody>();
+        hookGrabbed = false;
+        hookActive = false;
+        wasGrabbed = false;
+        currentSpeed = Vector3.zero;
         _canJump = true;
+        jetpackFuel = maxFuel;
         Cursor.visible = false;
     }
 
@@ -38,37 +67,169 @@ public class PlayerMove : MonoBehaviour
         cy = Mathf.Clamp(cy, -90, 90);
         _camera.transform.rotation = Quaternion.Euler(cy, cx, 0);
 
-
-        if (Input.GetKey(KeyCode.Space) && _canJump)
+        if (Input.GetKey(KeyCode.Space))
         {
-            Jump();
+            if (_canJump)
+            {
+                Jump();
+            }
         }
 
-        if (Input.GetKey(KeyCode.W))
+        if (_canJump)
         {
-            transform.Translate(0, 0, _speed * Time.deltaTime);
+            currentSpeed = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                currentSpeed += new Vector3(Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.y), 0, Mathf.Cos(Mathf.Deg2Rad * transform.rotation.eulerAngles.y));
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                currentSpeed -= new Vector3(Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.y), 0, Mathf.Cos(Mathf.Deg2Rad * transform.rotation.eulerAngles.y));
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                currentSpeed -= new Vector3(Mathf.Sin(Mathf.Deg2Rad * (90 + transform.rotation.eulerAngles.y)), 0, Mathf.Cos(Mathf.Deg2Rad * (90 + transform.rotation.eulerAngles.y)));
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                currentSpeed += new Vector3(Mathf.Sin(Mathf.Deg2Rad * (90 + transform.rotation.eulerAngles.y)), 0, Mathf.Cos(Mathf.Deg2Rad * (90 + transform.rotation.eulerAngles.y)));
+            }
+            currentSpeed = _speed * currentSpeed.normalized;
+            rb.velocity = currentSpeed;
         }
-        if (Input.GetKey(KeyCode.S))
+        else
         {
-            transform.Translate(0, 0, _speed * Time.deltaTime * -1);
+            rb.AddForce(0, -gravityPower, 0, ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.A))
+        //print($"{Input.GetMouseButtonDown(1)} && {!hookActive} && {Time.realtimeSinceStartup - cooldownTimer > cooldownTime}");
+        if (Input.GetMouseButtonDown(1) && !hookActive && Time.realtimeSinceStartup - cooldownTimer > cooldownTime)
         {
-            transform.Translate(_speed * Time.deltaTime * -1, 0, 0);
+            hookActive = true;
+            hook.SetActive(true);
+            rope.gameObject.SetActive(true);
+            hookDirection = _camera.transform.forward;
+            hook.transform.position = gameObject.transform.position + hookDirection * hookSpeed * Time.deltaTime;
+            hook.transform.rotation = _camera.transform.rotation;
+            rope.SetPosition(0, gameObject.transform.position);
+            rope.SetPosition(1, hook.transform.position);
+            releaseTimer = Time.realtimeSinceStartup;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (hookActive)
         {
-            transform.Translate(_speed * Time.deltaTime, 0, 0);
+            rope.SetPosition(0, gameObject.transform.position);
+            rope.SetPosition(1, hook.transform.position);
+            if (hookGrabbed != wasGrabbed)
+            {
+                hookLenght = (hook.transform.position - gameObject.transform.position).magnitude;
+                wasGrabbed = true;
+            }
+            if (!hookGrabbed)
+            {
+                hook.transform.position += hookDirection * hookSpeed * Time.deltaTime;
+                if ((hook.transform.position - gameObject.transform.position).magnitude > hookMaxLenght)
+                {
+                    rope.gameObject.SetActive(false);
+                    hook.SetActive(false);
+                    hookActive = false;
+                    cooldownTimer = Time.realtimeSinceStartup;
+                }
+            }
+            else
+            {
+                print($"{hookMaxLenght}, {hookLenght}, {(hook.transform.position - gameObject.transform.position).magnitude}");
+                if ((hook.transform.position - gameObject.transform.position).magnitude >= hookLenght)
+                {
+                    /*
+                    angle = Vector3.Angle(rb.velocity, hook.transform.position - gameObject.transform.position);
+                    if (angle > 90)
+                    {
+                        rb.velocity = Vector3.Slerp(rb.velocity, hook.transform.position - gameObject.transform.position, angle - 90) +
+                            hook.transform.position + (gameObject.transform.position - hook.transform.position).normalized * hookLenght - gameObject.transform.position;
+                    }
+                    else
+                    {
+                        rb.velocity += hook.transform.position + (gameObject.transform.position - hook.transform.position).normalized * hookLenght - gameObject.transform.position;
+                    }
+                    */
+                    rb.velocity += hook.transform.position + (gameObject.transform.position - hook.transform.position).normalized * hookLenght - gameObject.transform.position;
+                }
+            }
+            if (Input.GetMouseButtonDown(1) && Time.realtimeSinceStartup - releaseTimer > 1)
+            {
+                hookGrabbed = false;
+                hookActive = false;
+                wasGrabbed = false;
+                rope.gameObject.SetActive(false);
+                hook.SetActive(false);
+                cooldownTimer = Time.realtimeSinceStartup;
+            }
         }
+
+        if (!_canJump)
+        {
+            if (jetpackFuel > 0)
+            {
+                JetpackFly();
+            }
+        }
+        else
+        {
+            if (jetpackFuel < maxFuel)
+            {
+                jetpackFuel += Time.deltaTime;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        
     }
 
     void Jump()
     {
-        if (Input.GetKey(KeyCode.Space) && _canJump == true)
-        {
-            _canJump = false;
-            GetComponent<Rigidbody>().AddForce(0, 400, 0);
-        }
+        _canJump = false;
+        rb.AddForce(0, jumpPower, 0, ForceMode.Impulse);
     }
 
+    void JetpackFly()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(0, jetpackPower * Time.deltaTime, 0, ForceMode.Impulse);
+            jetpackFuel -= 5f * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            rb.velocity -= currentSpeed;
+            lastSpeed = currentSpeed;
+            currentSpeed = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                currentSpeed += new Vector3(Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.y), 0, Mathf.Cos(Mathf.Deg2Rad * transform.rotation.eulerAngles.y));
+                jetpackFuel -= 5 * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                currentSpeed -= new Vector3(Mathf.Sin(Mathf.Deg2Rad * transform.rotation.eulerAngles.y), 0, Mathf.Cos(Mathf.Deg2Rad * transform.rotation.eulerAngles.y));
+                jetpackFuel -= 5 * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                currentSpeed -= new Vector3(Mathf.Sin(Mathf.Deg2Rad * (90 + transform.rotation.eulerAngles.y)), 0, Mathf.Cos(Mathf.Deg2Rad * (90 + transform.rotation.eulerAngles.y)));
+                jetpackFuel -= 5 * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                currentSpeed += new Vector3(Mathf.Sin(Mathf.Deg2Rad * (90 + transform.rotation.eulerAngles.y)), 0, Mathf.Cos(Mathf.Deg2Rad * (90 + transform.rotation.eulerAngles.y)));
+                jetpackFuel -= 5 * Time.deltaTime;
+            }
+            currentSpeed = jetpackHorizontalPower * currentSpeed.normalized;
+            if (currentSpeed == Vector3.zero)
+            {
+                currentSpeed = lastSpeed;
+            }
+            rb.velocity += currentSpeed;
+        }
+    }
 }
