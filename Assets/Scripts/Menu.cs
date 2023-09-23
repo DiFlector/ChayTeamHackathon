2c4637f9fcb4ccbe7fb1aae58c5fa8720f25d7f1
DiFlector <egorgorbachenkov@gmail.com>
2023-09-23 21:05:22 +0300

    Menu done

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-
-    public void StartGame()
+    public int sceneID;
+    public void StartGame(int sceneID)
     {
-        Application.LoadLevelAsync(1);
+        StartCoroutine(LoadYourAsyncScene(sceneID));
     }
 
     public void Exit()
     {
         Application.Quit();
     }
-    
+
+    IEnumerator LoadYourAsyncScene(int sceneID)
+    {
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneID);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
 }
