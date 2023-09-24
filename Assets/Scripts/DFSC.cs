025720a1643454284b3f1e68499cc9835a787b70
DiFlector <egorgorbachenkov@gmail.com>
2023-09-24 08:40:55 +0300

    Finaly lvl3

--- Assets/Scripts/DFSC.cs
+++ Assets/Scripts/DFSC.cs
@@ -24,59 +24,16 @@ public class DFSC : MonoBehaviour
 
     private void Start()
     {
-        
+        StartCoroutine(Rand());
     }
 
     private void Update()
     {
-        
+
     }
     private void FixedUpdate()
     {
-        switch (Random.Range(0, 11))
-        {
-            case 10:
-                skyRotateDir = true;
-                break;
-            case 9:
-                worldRotateSpeed = Random.Range(1.0f, 2.0f);
-                break;
-            case 8:
-                skyRotateDir = false;
-                break;
-            case 7:
-                worldRotateDir = false;
-                break;
-            case 6:
-                worldRotateDir = true;
-                break;
-            case 5:
-                rotateWorld = true;
-                break;
-            case 4:
-                rotateWorld = false;
-                break;
-            case 3:
-                dimension = false;
-                break;
-            case 2:
-                dimension = true;
-                break;
-            case 1:
-                skyRotateSpeed = Random.Range(3.0f, 6.0f);
-                break;
-            default:
-                break;
-        }
-
-
-        if (Random.Range(0, 10) == 5)
-            
-        if (Random.Range(0, 10) == 6)
-            
-        if(Random.Range(0, 10) == 7)
-            
-        if(Random.Range(0, 10) == 5)
+        
         
         if(skyRotateDir)
             i = i + 1f;
@@ -94,5 +51,42 @@ public class DFSC : MonoBehaviour
             SpaceWorld.transform.rotation *= Quaternion.Euler(0, -j * Time.fixedDeltaTime * worldRotateSpeed, 0);
 
         RenderSettings.skybox.SetFloat("_Rotation", i * Time.fixedDeltaTime * skyRotateSpeed);
+
+        
+    }
+
+    IEnumerator Rand()
+    {
+        print(1);
+        yield return new WaitForSeconds(1);
+        print(2);
+        switch (Random.Range(0, 9))
+        {
+            case 8:
+                skyRotateDir = true;
+                break;
+            case 7:
+                worldRotateSpeed = Random.Range(1.0f, 25.0f);
+                break;
+            case 6:
+                skyRotateDir = false;
+                break;
+            case 5:
+                worldRotateDir = false;
+                break;
+            case 4:
+                worldRotateDir = true;
+                break;
+            case 3:
+                dimension = false;
+                break;
+            case 2:
+                dimension = true;
+                break;
+            default:
+                break;
+        }
+        print(3);
+        StartCoroutine(Rand());
     }
 }
